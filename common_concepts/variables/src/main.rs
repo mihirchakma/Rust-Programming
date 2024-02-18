55e3ea1c7f919d92c20ea760688ff536b52bd6cd
Mihir Chakma <mihirchakmaths@gmail.com>
2024-02-18 10:47:17 +0530

    add variables

---
+++ common_concepts/variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    // In Rust, Variables are immutable by default
+    // to change/mutable the variable's value, we use mut 
+    let mut x = 5;
+    println!("The value of x is {}", x);
+
+    x = 10;
+    println!("The value of x is {}", x);
+
+}
