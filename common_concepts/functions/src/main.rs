7a109c57d38057ab1e3fe191ee7b33da9328a27b
Mihir Chakma <mihirchakmaths@gmail.com>
2024-02-22 20:44:23 +0530

    update (#functions)

--- common_concepts/functions/src/main.rs
+++ common_concepts/functions/src/main.rs
@@ -5,6 +5,24 @@ fn main() {
     say_hello();
     another_function(10);
     print_date_month_year(21, "February".to_string(), 2024);
+
+
+    // Statements and Expressions
+
+    let a = 6; // statement 
+
+    println!("\n{a}");
+
+    let y = {
+        let x = 5; // expression
+        x + 5
+    }; // note this ; semicolon 
+
+    println!("The value of y is: {y}");
+
+
+    let call_five = five();
+    println!("\nThe value of function's return type is : {call_five}");
 }
 
 // Creating Functions 
@@ -18,6 +36,13 @@ fn another_function(x: i32) {
     println!("The value of x is {x}");
 }
 
+// create a function with parameters (arguments)
 fn print_date_month_year(date:i32, month: String, year: i64) {
     println!("\nDate: {date} \nMonth: {month} \nYear : {year}.");
+}
+
+
+// Functions with Return Values
+fn five() -> i32 {
+    5
 }
