f96b1e5c7b0c5ce90c6d0cf2fcc53a84554ccd21
Mihir Chakma <mihirchakmaths@gmail.com>
2024-02-23 12:42:00 +0530

    add (#if_expression)

---
+++ common_concepts/control_flow/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+    // if Expressions
+    let number = 5;
+
+    if number < 10 {
+        println!("Condition is true");
+    } else {
+        println!("Condition is false");
+    }
+
+    let num = 2;
+
+    if num != 0 {
+        println!("Condition is true");
+    }
+}
