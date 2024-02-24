f4506a4a206ccc42c25d5532332db752b02461ae
Mihir Chakma <mihirchakmaths@gmail.com>
2024-02-24 22:46:37 +0530

    update (#else if condition)

--- common_concepts/control_flow/src/main.rs
+++ common_concepts/control_flow/src/main.rs
@@ -9,9 +9,24 @@ fn main() {
         println!("Condition is false");
     }
 
+    // Rust conditions must be booleans 
+    // if there is not bool, it will throws an error
     let num = 2;
 
-    if num != 0 {
-        println!("Condition is true");
+    if num != 0 { // if num {}
+        println!("\nCondition is true");
+    }
+
+    // Handling Multiple Conditions with else if
+    let number_1 = 6;
+
+    if number_1 % 4 == 0 {
+        println!("\nNumber is divisible by 4");
+    } else if number_1 % 3 == 0 {
+        println!("\nnumber is divisible by 3");
+    } else if number_1 % 2 == 0 {
+        println!("\nnumber is divisible by 2");
+    } else {
+        println!("\nnumber is not divisible by 4, 3, or 2");
     }
 }
