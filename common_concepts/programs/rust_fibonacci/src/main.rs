4bc9fb10273fd4afeb844fd509d104134a74028a
Mihir Chakma <mihirchakmaths@gmail.com>
2024-03-21 11:18:39 +0530

    update (#rust_fibonacci)

--- common_concepts/programs/rust_fibonacci/src/main.rs
+++ common_concepts/programs/rust_fibonacci/src/main.rs
@@ -1,11 +1,27 @@
+use std::io;
+
 fn main() {
 
-    for int in 0..15 {
-        println!("Fibonacci ({}) => {}", int , fib(int));
+    println!("To quite the program type -> `exit` ");
+
+    loop {
+        println!("Enter a positive number: ");
+        let mut int_number = String::new();
+        io::stdin().read_line(&mut int_number).expect("Failed to read your input");
+        
+        if int_number.trim() == "exit" {
+            break;
+        }
+        let int_num: u32 = match int_number.trim().parse() {
+            Ok(int_num) => int_num,
+            Err(_) => continue,
+        };
+
+        println!("Fibonacci ({}) => {}", int_num , fib(int_num));
     }
 }
 
-fn fib(n: i32) -> i32 {
+fn fib(n: u32) -> u32 {
     if n <= 0 {
         return 0;
     } else if n == 1 {
